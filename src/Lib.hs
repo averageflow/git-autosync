fece4cfe52a61cd90dc180ea48f54398f7bbd596
averageflow <jjbigorra@gmail.com>
2022-04-03 14:27:42 +0200

    This is a programmatic commit, made with Haskell code, on 2022-04-03 14:27:42.863129188 CEST

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,20 +3,11 @@ module Lib
   )
 where
 
-import qualified Data.Functor
-import Data.Time (getZonedTime)
-import Data.Time.Calendar
-import Data.Time.Clock
-import GHC.Base (IO (IO))
-import GHC.IO.Exception (ExitCode (ExitFailure, ExitSuccess))
-import System.Process (readProcessWithExitCode)
-import Text.Printf
+import Git (areThereUncommittedChanges, commitChanges, pushChanges)
 
 data AutoSynchronizerActionTrigger = SyncOnUncommittedChanges | SyncOnDiffWithBranch
 
 cheapSeparator = "+-------------------------------------------------+"
-
-getCurrentDateTime = fmap show getZonedTime
 
 gitAutoSynchronizer = do
   putStrLn cheapSeparator
@@ -48,20 +39,3 @@ initiateAction SyncOnUncommittedChanges = do
       print stdErr
 -- Unimplemented conditions
 initiateAction _ = putStrLn "Unimplemented feature!"
-
-areThereUncommittedChanges :: IO Bool
-areThereUncommittedChanges = do
-  (shouldProceedToSync, _, _) <- readProcessWithExitCode "git" ["diff", "--quiet"] ""
-  if shouldProceedToSync == ExitSuccess
-    then return False
-    else return True
-
-commitMessage = do
-  dateTime <- getCurrentDateTime
-  return ("This is a programmatic commit, made with Haskell code, on " ++ dateTime)
-
-commitChanges = do
-  generatedCommitMessage <- commitMessage
-  readProcessWithExitCode "git" ["commit", "-am", generatedCommitMessage] ""
-
-pushChanges = readProcessWithExitCode "git" ["push"] ""
