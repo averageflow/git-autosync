a2cf6a62a7dacf53424e74852f4ba6691c4ed966
Josep Bigorra <josep.bigorra@ingka.ikea.com>
2022-04-06 13:38:11 +0200

    2022-04-06 13:38:11.046891351 CEST Programmatic commit from Haskell code

--- src/Lib.hs
+++ src/Lib.hs
@@ -45,25 +45,27 @@ beginSync config = do
     then do
       putStrLn "Adding changes..."
       processOutput <- addAllChanges . addPreferences . servicePreferences $ config
-      processPrettyPrinter processOutput
+      processOutputHandler processOutput
     else putStrLn "No additional changes will be added to VCS"
 
   putStrLn "Committing changes..."
   processOutput <- commitChanges . commitPreferences . servicePreferences $ config
-  processPrettyPrinter processOutput
+  processOutputHandler processOutput
 
   if pushToRemoteAfterCommit . pushPreferences . servicePreferences $ config
     then do
       putStrLn "Pushing changes..."
       processOutput <- pushChanges . pushPreferences . servicePreferences $ config
-      processPrettyPrinter processOutput
+      processOutputHandler processOutput
     else putStrLn "Will not push to remote due to user's configuration"
 
-processPrettyPrinter :: (ExitCode, String, String) -> IO ()
-processPrettyPrinter processOutput = do
+processOutputHandler :: (ExitCode, String, String) -> IO ()
+processOutputHandler processOutput = do
   let (exitCode, stdOut, stdErr) = processOutput
   case exitCode of
     ExitSuccess -> do
       putStrLn stdOut
     _ -> do
       putStrLn stdErr
+      putStrLn "Aborting operation! Please see the above errors!"
+      exitFailure
